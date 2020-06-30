9da09188e40cfa45dfbb841b66ec9cc9cf2653d2
mspstead <mspstead@gmail.com>
2020-06-30 18:01:08 +0100

    Updated text in modals

--- static/assets/js/modals.js
+++ static/assets/js/modals.js
@@ -12,7 +12,7 @@ $("#input-button").click(function(){
 });
 $('input:radio[name="text-options"]').change(function(){
     if($(this).val() == 'comments'){
-        document.getElementById("input-type-label").innerHTML = "Input type: Reddit comments";
+        document.getElementById("input-type-label").innerHTML = "Input type: Reddit comments URL";
         document.getElementById("text-input").style.display = "none";
         document.getElementById("reddit-input").style.display = "block";
     }
